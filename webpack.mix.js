80fde5c2c8bce1c288422e65a5c393bac8345285
Ryo Kobayashi <cuba6vst@gmail.com>
2019-07-04 16:54:09 +0900

    fix laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,7 @@ const mix = require("laravel-mix");
 
 mix
   .js("resources/js/app.js", "public/js")
-  .sass("resources/sass/app.scss", "public/css");
+  .sass("resources/sass/app_dev.scss", "public/css/app.css");
 if (mix.inProduction()) {
-  mix.sass("resources/sass/fa.scss", "public/css/app.css").version();
+  mix.sass("resources/sass/app_prod.scss", "public/css/app.css").version();
 }
